8e16deafdb7e74441eb2b090e9a9fcf3ae2c9808
Sajmon9696 <Sajmon9696@noreply.github.com>
2024-01-24 11:45:51 +0100

    migrate models form car app and register them

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+
+admin.site.register(models.Fuel)
+admin.site.register(models.Color)
+admin.site.register(models.Brand)
+admin.site.register(models.CarClass)
+admin.site.register(models.EngineSize)
+admin.site.register(models.CarModel)
+admin.site.register(models.Car)
