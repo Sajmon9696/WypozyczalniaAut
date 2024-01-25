050c07829a672c5893d9f2fee8706eda44af85af
Sajmon9696 <Sajmon9696@noreply.github.com>
2024-01-25 10:38:29 +0100

    created reservation model and register it

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from cars.models import Car
+
+
 # Create your models here.
+
+
+class Reservation(models.Model):
+    reservation_start_date = models.DateField()
+    reservation_end_date = models.DateField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_reservation')
+    user = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='user_reservation')
