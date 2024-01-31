064a3400001d71981f66681df790e10837cd93fd
th3s1nn3rpl <th3s1nn3rpl@gmail.com>
2024-01-31 20:03:13 +0100

    dodane wyświetlanie dla admina

--- cars/models.py
+++ cars/models.py
@@ -5,29 +5,33 @@ from django.db import models
 
 class Color(models.Model):
     color_name = models.CharField(max_length=64)
-
+    def __str__(self): return f"{self.color_name}"
 
 class Fuel(models.Model):
     fuel_name = models.CharField(max_length=64)
-
+    def __str__(self): return f"{self.fuel_name}"
 
 class EngineSize(models.Model):
     engin_size_name = models.CharField(max_length=64)
+    def __str__(self): return f"{self.engin_size_name}"
 
 
 class Brand(models.Model):
     brand_name = models.CharField(max_length=64)
+    def __str__(self): return f"{self.brand_name}"
 
 
 class CarClass(models.Model):
     car_class_name = models.CharField(max_length=64)
     price = models.PositiveIntegerField()
+    def __str__(self): return f"{self.car_class_name}"
 
 
 class CarModel(models.Model):
     model_name = models.CharField(max_length=64)
     car_class = models.ForeignKey(CarClass, on_delete=models.CASCADE)
     car_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    def __str__(self): return f'{self.car_brand}{self.model_name}'
 
 
 class Car(models.Model):
@@ -35,3 +39,4 @@ class Car(models.Model):
     fuel = models.ForeignKey(Fuel, on_delete=models.CASCADE)
     engine_size = models.ForeignKey(EngineSize, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    def __str__(self): return f'{self.model}'
