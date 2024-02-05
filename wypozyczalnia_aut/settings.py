7861068a9362efb24cd0f893c032707bcec0adec
th3s1nn3rpl <th3s1nn3rpl@gmail.com>
2024-02-05 18:07:24 +0100

    dodano możliwość wstawienia obrazka do auta

--- wypozyczalnia_aut/settings.py
+++ wypozyczalnia_aut/settings.py
@@ -123,6 +123,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'cars:all_cars'
 
-#import os
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
