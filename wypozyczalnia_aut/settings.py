88cd697f130151e776d12008af2dc461e0ed6ace
Sajmon9696 <Sajmon9696@noreply.github.com>
2024-02-05 18:47:50 +0100

    Merge branch 'main' of https://github.com/Sajmon9696/WypozyczalniaAut
    # Conflicts:
    #	Base_app/templates/Base_app/main.html
    #	wypozyczalnia_aut/settings.py

--- wypozyczalnia_aut/settings.py
+++ wypozyczalnia_aut/settings.py
@@ -120,3 +120,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'cars:all_cars'
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
