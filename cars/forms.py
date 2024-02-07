deb5c97c92dbad3aefc4b972bc8b5f75735e4eb5
Sajmon9696 <Sajmon9696@noreply.github.com>
2024-02-07 19:09:54 +0100

    powrot do poprzedniego

--- cars/forms.py
+++ cars/forms.py
@@ -8,21 +8,13 @@ class CarFilterForm(ModelForm):
     class Meta:
         model = Car
         fields = ['model', 'fuel', 'engine_size', 'color']
-        widgets = {
-            'color': forms.CheckboxSelectMultiple(),
-            'fuel': forms.CheckboxSelectMultiple(),
-            'engine_size': forms.CheckboxSelectMultiple(),
-            'model': forms.CheckboxSelectMultiple(),
-        }
-        required = {
-            'color': False,
-            'fuel': False,
-            'engine_size': False,
-            'model': False,
-        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['color'].required = False
+        self.fields['fuel'].required = False
+        self.fields['engine_size'].required = False
+        self.fields['model'].required = False
         self.fields['car_class'] = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                                              choices=[(price.id, price.car_class_name) for price in
                                                                       CarClass.objects.all()], required=False)
